7e79124e85c3ba59e386c51baeae6fa236c8d0d5
Nick Babcock <nbabcock19@hotmail.com>
2016-02-24 20:26:44 -0500

    Start of something

--- js/components/Home.js
+++ js/components/Home.js
@@ -4,17 +4,20 @@ import {bindActionCreators} from 'redux';
 import * as HomeActions from '../actions/HomeActions';
 import AppBar from 'material-ui/lib/app-bar';
 import styles from '../../css/app.css';
+import Map from './Map';
 
 class Home extends Component {
   render() {
     const {title, dispatch} = this.props;
     const actions = bindActionCreators(HomeActions, dispatch);
+
     return (
       <main>
         <AppBar
           title="Umich DPS"
           iconClassNameRight="muidocs-icon-navigation-expand-more"
         />
+        <Map/>
         <h1 className={styles.text}>Welcome {title}!</h1>
         <button onClick={e => actions.changeTitle(prompt())}>
           Update Title
@@ -24,4 +27,8 @@ class Home extends Component {
   }
 }
 
-export default connect(state => state.Sample)(Home)
+function select(state) {
+  return state.Sample;
+}
+
+export default connect(select)(Home)
---
+++ js/components/Map.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import {Gmaps, Marker, InfoWindow, Circle} from 'react-gmaps';
+
+export default class Map extends Component {
+  onMapCreated(map) {
+    map.setOptions({
+      disableDefaultUI: true
+    });
+  }
+
+  render() {
+    const { coords } = this.props;
+    return (
+      <Gmaps
+        width={'800px'}
+        height={'600px'}
+        lat={coords.lat}
+        lng={coords.lng}
+        zoom={12}
+        loadingMessage={'Be happy'}
+        params={{v: '3.exp'}}
+        onMapCreated={this.onMapCreated}>
+      </Gmaps>
+    );
+  }
+}
+
+Map.defaultProps = {
+  coords: {
+    lat: 51.5258541,
+    lng: -0.08040660000006028
+  }
+};
+
