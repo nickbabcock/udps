b26d3e6f9e24cb061aebfc5c3c4e35d5f7817d71
Nick Babcock <nbabcock19@hotmail.com>
2016-02-25 19:55:43 -0500

    Add localforage for caching

--- js/actions/HomeActions.js
+++ js/actions/HomeActions.js
@@ -1,5 +1,7 @@
-import fetch from 'isomorphic-fetch'
-import {TITLE_CHANGED} from '../constants/ActionTypes';
+import fetch from 'isomorphic-fetch';
+import * as localforage from 'localforage';
+const moment = require('moment');
+import { TITLE_CHANGED, REQUEST_DPS, REQUEST_DPS_DONE } from '../constants/ActionTypes';
 
 export function changeTitle(text) {
   return {
@@ -8,9 +10,20 @@ export function changeTitle(text) {
   }
 }
 
+function fetchData(dispatch) {
+  dispatch({ type: REQUEST_DPS });
+  return fetch('/app.json').then(x => x.json());
+}
+
 export function fetchPostsIfNeeded() {
   return async dispatch => {
-    const data = await fetch('/app.json').then(x => x.json());
+    const lastUpdate = await localforage.getItem('last-update');
+    const data = !lastUpdate || !moment().isSame(lastUpdate, 'day') ?
+      await fetchData(dispatch) :
+      await localforage.getItem('data');
+
+    await localforage.setItem('last-update', moment().format());
+    await localforage.setItem('data', data);
     dispatch({ type: 'hello', data });
   }
 }
