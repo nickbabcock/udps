e81dbcf8f29608d6774e67ddabf802a60d9ffd4a
Nick Babcock <nbabcock19@hotmail.com>
2016-03-05 15:23:42 -0500

    Add close button

--- js/components/Welcome.js
+++ js/components/Welcome.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import Message from './Welcome.md';
 import styles from './Welcome.css';
+import FlatButton from 'material-ui/lib/flat-button';
 
 export default class Welcome extends Component {
   markup() {
@@ -13,6 +14,9 @@ export default class Welcome extends Component {
       <div className={styles.leader}>
         <div className={styles.largeFont}
              dangerouslySetInnerHTML={this.markup()}/>
+        <FlatButton label="x"
+           style={{ alignSelf: 'flex-start', marginTop: '14px' }}
+           onClick={(e) => alert('ehlo')}/>
       </div>
     )
   }
