3255632b470ad9d84aff4b88819979a607a3f746
Nick Babcock <nbabcock19@hotmail.com>
2016-03-07 21:20:41 -0500

    About message page

--- js/components/About.js
+++ js/components/About.js
@@ -1,9 +1,16 @@
 import React, {Component} from 'react';
+import Message from './About.md';
 
 export default class About extends Component {
+  markup() {
+    return {
+      __html: Message
+    };
+  }
+
   render() {
     return (
-      <div>About</div>
+      <div dangerouslySetInnerHTML={this.markup()}/>
     );
   }
 }
