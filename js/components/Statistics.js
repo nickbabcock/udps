7fe09ae14e653a6b4628c8814ffb6d08acce8500
Nick Babcock <nbabcock19@hotmail.com>
2016-03-14 12:24:22 -0400

    Update statistics page to use header

--- js/components/Statistics.js
+++ js/components/Statistics.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react';
 export default class Statistics extends Component {
   render() {
     return (
-      <div>Statistics</div>
+      <h2>Statistics</h2>
     );
   }
 }
