b2d3abddb01b2b08c94ab9174e2df81fce7c4af8
Nick Babcock <nbabcock19@hotmail.com>
2016-03-24 08:24:52 -0400

    Add proptypes onto App component

--- js/containers/App.js
+++ js/containers/App.js
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import { Provider, connect } from 'react-redux';
+import React, { PropTypes } from 'react';
 import { Router, Route, IndexRoute } from 'react-router';
 import Home from '../components/Home';
 import DevTools from './DevTools';
@@ -7,22 +6,23 @@ import Statistics from '../components/Statistics';
 import About from '../components/About';
 import InnerApp from '../components/InnerApp';
 
-export default class App extends Component {
-  render() {
-    const { history } = this.props;
-    return (
-      <div style={{ minHeight: '100%', height: '100%' }}>
-        <Router history={history}>
-          <Route path="/" component={InnerApp}>
-            <IndexRoute component={Home} />
-            <Route path="/statistics" component={Statistics} />
-            <Route path="/about" component={About} />
-          </Route>
-        </Router>
-        {/* only renders when running in DEV mode */
-          <DevTools />
-        }
-      </div>
-    );
-  }
-}
+const App = ({ history }) => (
+  <div style={{ minHeight: '100%', height: '100%' }}>
+    <Router history={history}>
+      <Route path="/" component={InnerApp}>
+        <IndexRoute component={Home} />
+        <Route path="/statistics" component={Statistics} />
+        <Route path="/about" component={About} />
+      </Route>
+    </Router>
+    {/* only renders when running in DEV mode */
+      <DevTools />
+    }
+  </div>
+);
+
+App.propTypes = {
+  history: PropTypes.object.isRequired
+};
+
+export default App;
