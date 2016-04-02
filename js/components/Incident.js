8ee3bd3a59a4003bcb1f575ce05432a03f2f651e
Nick Babcock <nbabcock19@hotmail.com>
2016-04-01 21:30:12 -0400

    Start of animations!

---
+++ js/components/Incident.js
@@ -0,0 +1,28 @@
+import React, { PropTypes } from 'react';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group';
+import styles from './Incident.css';
+import GridTile from 'material-ui/lib/grid-list/grid-tile';
+
+const Incident = ({ incident }) => (
+  <ReactCSSTransitionGroup
+    transitionName={styles}
+    transitionEnterTimeout={1000}
+    transitionLeaveTimeout={1000}
+    transitionAppear
+    transitionAppearTimeout={500}
+  >
+      <GridTile
+        key={incident.id}
+        title={incident.description}
+        subtitle={incident.address}
+      >
+      <div className={styles.incident} />
+    </GridTile>
+  </ReactCSSTransitionGroup>
+);
+
+Incident.propTypes = {
+  incident: PropTypes.object.isRequired
+};
+
+export default Incident;
--- js/containers/Home.js
+++ js/containers/Home.js
@@ -1,9 +1,9 @@
 import React, { Component, PropTypes } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
-import GridTile from 'material-ui/lib/grid-list/grid-tile';
 import DatePicker from 'material-ui/lib/date-picker/date-picker';
 import Welcome from '../components/Welcome';
+import Incident from '../components/Incident';
 import styles from './Home.css';
 import * as HomeActions from '../actions/HomeActions';
 import { getSelectedData } from '../selectors';
@@ -33,13 +33,7 @@ class Home extends Component {
         { welcomeMessage !== false ? <Welcome closeFn={closeFn} /> : null }
         <div className={styles['incident-container']}>
             {selectedData.map((x) =>
-              <GridTile
-                key={x.id}
-                title={x.description}
-                subtitle={x.address}
-              >
-                <div className={styles.incident} />
-              </GridTile>
+              <Incident key={x.id} incident={x} />
             )}
         </div>
       </main>
