efbeb43b1a089517ca5f4f7f39acfafdab3e3184
Nick Babcock <nbabcock19@hotmail.com>
2016-04-09 19:24:35 -0400

    Document some code

--- js/components/Map.js
+++ js/components/Map.js
@@ -1,12 +1,13 @@
 import React, { PropTypes } from 'react';
 
+// Use google static maps api
+// https://developers.google.com/maps/documentation/static-maps/intro
+// to render incident locations and let google geocode the addresses.
 const Map = ({ address }) => {
   const url = '//maps.googleapis.com/maps/api/staticmap';
   const size = '250x250';
   const src = `${url}?size=${size}&markers=${escape(`|${address}, Ann Arbor, MI`)}`;
-  return (
-      <img src={src} />
-  );
+  return <img src={src} />;
 };
 
 Map.propTypes = {
--- js/selectors/index.js
+++ js/selectors/index.js
@@ -2,6 +2,8 @@ import { createSelector } from 'reselect';
 import { head, last, partition, sortBy, compact } from 'lodash';
 const moment = require('moment');
 
+// If someone navigates to the page like /2016-01-01, that takes precedent over
+// any previous state
 const getDate = (state, props) =>
   (!props.params.incidentDate ? state.date :
     moment(props.params.incidentDate).toDate());
@@ -9,12 +11,16 @@ const getDate = (state, props) =>
 const getData = (state) => state.data;
 
 export const getSelectedDate = createSelector([getDate], (date) => date);
+
+// Filter to incidents that occurred as the same day that is selected
 export const getSelectedData = createSelector(
   [getData, getDate],
   (data, date) =>
     data.filter((x) => moment(x.date).isSame(date, 'day'))
 );
 
+// If no incidents occurred on a given day select the nearest past and future
+// date that where incidents occurred.
 export const getBetterDates = createSelector(
   [getSelectedData, getData, getDate],
   (selectedData, data, date) => {
