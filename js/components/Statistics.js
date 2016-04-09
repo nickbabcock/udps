ca6c5d22c751fd5553f81e15d50d1ac3df8c2747
Nick Babcock <nbabcock19@hotmail.com>
2016-04-09 11:27:58 -0400

    Signal that statistics page is under construction

--- js/components/Statistics.js
+++ js/components/Statistics.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import DatePicker from 'material-ui/lib/date-picker/date-picker';
 
 export default function Statistics() {
   return (
     <div>
       <h2>Statistics</h2>
-      <DatePicker hintText="Portrait Dialog" />
+      <p>This page is under construction!</p>
     </div>
   );
 }
