5c163fbd3879d29086d4eddafb1dba97cc26d1b5
Nick Babcock <nbabcock19@hotmail.com>
2016-04-09 14:51:30 -0400

    Lint tests

--- test/test.js
+++ test/test.js
@@ -4,18 +4,18 @@ const moment = require('moment');
 
 chai.use(require('chai-datetime'));
 
-describe('Selectors', function() {
-  describe('#getDate()', function() {
-    it('should return state date when param not specified', function() {
+describe('Selectors', function () {
+  describe('#getDate()', function () {
+    it('should return state date when param not specified', function () {
       const date = new Date();
-      expect(getSelectedDate({ date }, {params: {}})).to.equal(date);
+      expect(getSelectedDate({ date }, { params: {} })).to.equal(date);
     });
 
-    it('should return param when param is specified', function() {
+    it('should return param when param is specified', function () {
       const incidentDate = '2016-02-04';
       const state = { date: new Date() };
       const expected = moment(incidentDate).toDate();
-      expect(getSelectedDate(state, { params: { incidentDate }})).to.equalDate(expected);
+      expect(getSelectedDate(state, { params: { incidentDate } })).to.equalDate(expected);
     });
   });
 });
