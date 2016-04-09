70e5275ac4dd65ce1aed0bea6debcfe3e0e61085
Nick Babcock <nbabcock19@hotmail.com>
2016-04-08 20:22:47 -0400

    Remove unused geocode function

--- js/actions/HomeActions.js
+++ js/actions/HomeActions.js
@@ -3,7 +3,6 @@ import { browserHistory } from 'react-router';
 import fetch from 'isomorphic-fetch';
 import * as localforage from 'localforage';
 const moment = require('moment');
-import geocode from '../utils/geocode';
 import {
   REQUEST_DPS,
   REQUEST_DPS_DONE,
@@ -14,30 +13,6 @@ import {
 function fetchData(dispatch) {
   dispatch({ type: REQUEST_DPS });
   return fetch('/app.json').then((x) => x.json());
-}
-
-async function getLatLng(address) {
-  const addr = await localforage.getItem(address);
-  if (addr) {
-    return addr;
-  }
-
-  const addrReq = await geocode(`${address}, ANN ARBOR, MI`);
-  const result = {
-    lat: addrReq[0].geometry.location.lat(),
-    lng: addrReq[0].geometry.location.lng()
-  };
-
-  await localforage.setItem(address, result);
-  return result;
-}
-
-async function recent(data) {
-  const ndata = take(sortBy(data, (x) => -(+moment(x.date))), 10);
-  const addrs =
-    ndata.map((x) => getLatLng(x.address)
-      .then((geometry) => merge(x, { geometry })));
-  await Promise.all(addrs);
 }
 
 export function updateWelcomeMessage() {
@@ -75,7 +50,6 @@ export function fetchPostsIfNeeded() {
     let data = !lastUpdate || !moment().isSame(lastUpdate, 'day') ?
       await fetchData(dispatch) :
       await localforage.getItem('data');
-    recent(data);
     await localforage.setItem('last-update', moment().format());
     await localforage.setItem('data', data);
     data = data.map((x) => Object.assign(x, { date: moment(x.date) }));
