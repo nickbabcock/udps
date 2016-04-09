9018e8971c6c0e257ecc31c433b39ed01d62c172
Nick Babcock <nbabcock19@hotmail.com>
2016-04-09 14:46:06 -0400

    Test date selector when param is specified

--- test/test.js
+++ test/test.js
@@ -1,11 +1,21 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
 import { getSelectedDate } from '../js/selectors';
+const moment = require('moment');
+
+chai.use(require('chai-datetime'));
 
 describe('Selectors', function() {
   describe('#getDate()', function() {
     it('should return state date when param not specified', function() {
       const date = new Date();
       expect(getSelectedDate({ date }, {params: {}})).to.equal(date);
+    });
+
+    it('should return param when param is specified', function() {
+      const incidentDate = '2016-02-04';
+      const state = { date: new Date() };
+      const expected = moment(incidentDate).toDate();
+      expect(getSelectedDate(state, { params: { incidentDate }})).to.equalDate(expected);
     });
   });
 });
