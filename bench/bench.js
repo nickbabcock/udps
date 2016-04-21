987f63c5487989ffb3931b163c519650ab513a87
Nick Babcock <nbabcock19@hotmail.com>
2016-04-20 20:41:26 -0400

    Benchmark old and current version of getBetterDates

--- bench/bench.js
+++ bench/bench.js
@@ -1,14 +1,34 @@
-import { getBetterDates } from '../js/selectors';
+import { partition, compact, minBy, maxBy, head, last, sortBy } from 'lodash';
 const Benchmark = require('benchmark');
 const moment = require('moment');
-let data = require('../test-data.json');
 const suite = new Benchmark.Suite;
 
+// Conveniently grab a largish data set to benchmark the functions
+let data = require('../test-data.json');
 data = data.map(x => Object.assign({}, x, { date: moment(x.date) }));
-const state = { data, date: moment('2000-02-03') };
-const props = { params: {} };
 
-suite.add('getBetterDates', () => getBetterDates(state, props))
+const currentGetBetterDates = (data, date) => {
+    const [bf, af] = partition(data, (x) => x.date.isBefore(date, 'day'));
+    const result = [maxBy(bf, x => x.date), minBy(af, x => x.date)];
+    return compact(result).map(x => x.date);
+};
+
+// Has worse complexity because we first sort and then grab the first/last
+// element, which is needless work
+const oldGetBetterDates = (data, date) => {
+    const [bf, af] = partition(data, (x) => x.date.isBefore(date, 'day'));
+    const result = [
+      last(sortBy(bf.map((x) => x.date))),
+      head(sortBy(af.map((x) => x.date)))
+    ];
+    return compact(result);
+};
+
+// Cache the date so it is not constructed on each test run
+const date = moment('2000-02-03');
+
+suite.add('getBetterDates', () => currentGetBetterDates(data, date))
+.add('oldGetBetterDates', () => oldGetBetterDates(data, date))
 .on('complete', function() {
   for (let i = 0; i < this.length; i++) {
     console.log(`${this[i].name}: ${(this[i].stats.mean * 1000).toFixed(2)}ms`);
