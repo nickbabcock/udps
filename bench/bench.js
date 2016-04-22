d4622e2a57ce9d291aba679ee67bad037eaa762e
Nick Babcock <nbabcock19@hotmail.com>
2016-04-21 21:36:29 -0400

    Benchmark manual moment date comparison

--- bench/bench.js
+++ bench/bench.js
@@ -24,11 +24,18 @@ const oldGetBetterDates = (data, date) => {
     return compact(result);
 };
 
-// Cache the date so it is not constructed on each test run
+// Cache the dates so it is not constructed on each test run
 const date = moment('2000-02-03');
+const date2 = moment('2000-02-04');
+
+const sameDay = (d) => date.isSame(d, 'day');
+const sameDayManual = (d) => date.year() === d.year() &&
+  date.month() === d.month() && date.day() === d.month();
 
 suite.add('getBetterDates', () => currentGetBetterDates(data, date))
 .add('oldGetBetterDates', () => oldGetBetterDates(data, date))
+.add('sameDay', () => sameDay(date2))
+.add('sameDayManual', () => sameDayManual(date2))
 .on('complete', function() {
   for (let i = 0; i < this.length; i++) {
     console.log(`${this[i].name}: ${(this[i].stats.mean * 1000 * 1000).toFixed(2)}us`);
