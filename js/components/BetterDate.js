b3461499af45d92aac7a4e4af158263df4d6530b
Nick Babcock <nbabcock19@hotmail.com>
2016-04-24 22:00:21 -0400

    handle multiple better dates with proper english

--- js/components/BetterDate.js
+++ js/components/BetterDate.js
@@ -3,12 +3,25 @@ import { Link } from 'react-router';
 
 const BetterDate = ({ date, betterDates }) => (
   <div>
-    No incidents happened on {date.format('LL')}, how about trying the following:
-      {betterDates.map((x) =>
-        <Link key={x} to={`/date/${x.format('YYYY-MM-DD')}`}>
-          {x.format('LL')}
-        </Link>
-      )}
+    No incidents happened on {date.format('LL')}, how about trying the following:&nbsp;
+      {betterDates.map((x, i) => {
+        if (i === betterDates.length - 1) {
+          return (
+            <Link key={x} to={`/date/${x.format('YYYY-MM-DD')}`}>
+              {x.format('LL')}
+            </Link>
+          );
+        } else {
+          return (
+            <span>
+              <Link key={x} to={`/date/${x.format('YYYY-MM-DD')}`}>
+                {x.format('LL')}
+              </Link>
+              <span> and </span>
+            </span>
+          );
+        }
+      })}
   </div>
 );
 
--- test/BetterDate-test.js
+++ test/BetterDate-test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { expect } from 'chai';
-import { shallow } from 'enzyme';
+import { shallow, render } from 'enzyme';
 import { Link } from 'react-router';
 import BetterDate from '../js/components/BetterDate';
 const moment = require('moment');
@@ -12,11 +12,26 @@ describe('<BetterDate />', function () {
     const wrapper = shallow(<BetterDate date={date} betterDates={dates} />);
     expect(wrapper.text()).to.contain('No incidents happened on February 1, 2012');
   });
+
   it('should contain link with YYYY-MM-DD format', function () {
     const date = moment('2012-02-01');
     const dates = [moment('2012-02-03')];
     const wrapper = shallow(<BetterDate date={date} betterDates={dates} />);
     const link = wrapper.find(Link);
     expect(link.prop('to')).to.eql('/date/2012-02-03');
+  });
+
+  it('should handle single better date', function () {
+    const date = moment('2012-02-01');
+    const dates = [moment('2012-02-03')];
+    const wrapper = render(<BetterDate date={date} betterDates={dates} />);
+    expect(wrapper.text()).to.contain('February 3, 2012');
+  });
+
+  it('should handle multiple better dates', function () {
+    const date = moment('2012-02-01');
+    const dates = [moment('2012-01-31'), moment('2012-02-03')];
+    const wrapper = render(<BetterDate date={date} betterDates={dates} />);
+    expect(wrapper.text()).to.contain('January 31, 2012 and February 3, 2012');
   });
 });
