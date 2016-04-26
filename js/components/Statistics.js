4116ad5c05f4c2560dc0302d62492e6bf06632f5
Nick Babcock <nbabcock19@hotmail.com>
2016-04-25 21:56:20 -0400

    Start of charting

--- js/components/Statistics.js
+++ js/components/Statistics.js
@@ -1,10 +1,61 @@
-import React from 'react';
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { Line as LineChart } from 'react-chartjs';
+import * as HomeActions from '../actions/HomeActions';
+const moment = require('moment');
+import { groupBy } from 'lodash';
 
-export default function Statistics() {
-  return (
-    <div>
-      <h2>Statistics</h2>
-      <p>This page is under construction!</p>
-    </div>
-  );
+const months = (data) => {
+  console.log(groupBy(data, x => x.date.month()));
+  return groupBy(data, x => x.date.month())
 }
+
+class Statistics extends Component {
+  componentDidMount() {
+    const { actions } = this.props;
+    actions.fetchPostsIfNeeded();
+  }
+
+  render() {
+    const data = {
+       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+       datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3]
+          }]
+     };
+
+    const options = {
+      scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero: true
+                  }
+              }]
+          }
+    };
+    return (
+      <div>
+        <h2>Statistics</h2>
+        <LineChart options={options} data={data} width="600" height="250" />
+        <p>This page is under construction!</p>
+      </div>
+    );
+  }
+}
+
+Statistics.propTypes = {
+  actions: PropTypes.object.isRequired,
+  data: PropTypes.array.isRequired
+}
+
+const mapStateToProps = (state, ownProps) => ({
+  data: months(state.data)
+});
+
+const mapDispatchToProps = (dispatch) => ({
+  actions: bindActionCreators(HomeActions, dispatch)
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Statistics);
