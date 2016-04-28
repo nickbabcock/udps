35d42e9a2af58911218401ee05271af3e7788eb7
Nick Babcock <nbabcock19@hotmail.com>
2016-04-27 22:59:10 -0400

    Baby's first chart on incidents

--- js/components/Statistics.js
+++ js/components/Statistics.js
@@ -3,13 +3,14 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import { Line as LineChart } from 'react-chartjs';
 import * as HomeActions from '../actions/HomeActions';
-const moment = require('moment');
 import { groupBy } from 'lodash';
 
 const months = (data) => {
-  console.log(groupBy(data, x => x.date.month()));
-  return groupBy(data, x => x.date.month())
-}
+  const shortMonths = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+    'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+  const monthlyData = groupBy(data, x => x.date.month());
+  return shortMonths.map((x, i) => [x, monthlyData[i] || []]);
+};
 
 class Statistics extends Component {
   componentDidMount() {
@@ -18,27 +19,28 @@ class Statistics extends Component {
   }
 
   render() {
-    const data = {
-       labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-       datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3]
-          }]
-     };
+    const { data } = this.props;
+    const chartData = {
+      labels: data.map(([mon]) => mon),
+      datasets: [{
+        label: '# of Incidents',
+        data: data.map(([, val]) => val.length)
+      }]
+    };
 
     const options = {
       scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                  }
-              }]
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
           }
+        }]
+      }
     };
     return (
       <div>
         <h2>Statistics</h2>
-        <LineChart options={options} data={data} width="600" height="250" />
+        <LineChart options={options} data={chartData} width={"250"} />
         <p>This page is under construction!</p>
       </div>
     );
@@ -48,7 +50,7 @@ class Statistics extends Component {
 Statistics.propTypes = {
   actions: PropTypes.object.isRequired,
   data: PropTypes.array.isRequired
-}
+};
 
 const mapStateToProps = (state, ownProps) => ({
   data: months(state.data)
